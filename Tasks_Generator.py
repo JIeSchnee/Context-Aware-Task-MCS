9a1ab0a400b56838d4a5c47d03dc9cb036ede306
JIeSchnee <56720046+JIeSchnee@users.noreply.github.com>
2021-08-09 15:11:25 +0100

    Add files via upload

--- Tasks_Generator.py
+++ Tasks_Generator.py
@@ -8,10 +8,10 @@ from prettytable import PrettyTable
 class Task:
     criticality = ''
 
-    def __init__(self, task_id, job_id, release_time, deadline, period, execution_time_LO, execution_time_HI, priority,
+    def __init__(self, task_id, app_id, release_time, deadline, period, execution_time_LO, execution_time_HI, priority,
                  importance, criticality, context_aware):
         self.task_id = task_id
-        self.job_id = job_id
+        self.app_id = app_id
         self.release_time = release_time
         self.deadline = deadline
         self.period = period
@@ -52,12 +52,13 @@ def uunifast(stream_number, target_Uti):
 
 def task_definition(task_number, period, execution_time, criticality_factor, criticality_proportion):
     tasks = []
-    HI_task_num = math.floor(task_number * criticality_proportion)
+    HI_task_num = math.floor(task_number * criticality_proportion) - 1
+    # context aware task will be selected from LO group
     index_init = range(task_number)
     HI_task_index = sample(index_init, HI_task_num)
-    print("the index of HI-criticality tasks in one task set:", HI_task_index)
-    context_aware_index = sample(HI_task_index, 1)
-    print("the index of context-aware task in one task set:", context_aware_index)
+    # print("the index of HI-criticality tasks in one task set:", HI_task_index)
+    # context_aware_index = sample(HI_task_index, 1)
+    # print("the index of context-aware task in one task set:", context_aware_index)
 
     for i in range(task_number):
         if i in HI_task_index:
@@ -67,16 +68,16 @@ def task_definition(task_number, period, execution_time, criticality_factor, cri
             criticality = "LO"
             execution_time_HI = 0
 
-        if i in context_aware_index:
-            context_aware = 1
-        else:
-            context_aware = 0
+        # if i in context_aware_index:
+        #     context_aware = 1
+        # else:
+        #     context_aware = 0
 
         # release_time = random.randint(0, hyperperiod_calculation(period))
         release_time = 0
 
-        task = Task(i, 0, release_time, period[i], period[i], execution_time[i], execution_time_HI, i, i,
-                    criticality, context_aware)
+        task = Task(0, 0, release_time, period[i], period[i], execution_time[i], execution_time_HI, i, i,
+                    criticality, 0)
 
         tasks.append(task)
     return tasks
@@ -92,14 +93,28 @@ def hyperperiod_calculation(period):
 
 def table_print(tasks):
     table = PrettyTable(
-        ['task_id', 'job_id', 'release_time', 'deadline', 'period', 'execution_time_LO', 'execution_time_HI',
+        ['task_id', 'app_id', 'release_time', 'deadline', 'period', 'execution_time_LO', 'execution_time_HI',
          'priority',
          'importance', 'criticality', 'context-aware:'])
     for i in tasks:
         table.add_row(
-            [i.task_id, i.job_id, i.release_time, i.deadline, i.period, i.execution_time_LO, i.execution_time_HI,
+            [i.task_id, i.app_id, i.release_time, i.deadline, i.period, i.execution_time_LO, i.execution_time_HI,
              i.priority, i.importance, i.criticality, i.context_aware])
     print(table)
+
+
+def cmp(a):
+    return a.deadline
+
+
+def tasks_sort(app_tasks, app_id):
+    for i in app_tasks:
+        i.app_id = app_id
+    app_tasks.sort(key=cmp)
+    for i in range(len(app_tasks)):
+        app_tasks[i].task_id = i
+
+    return app_tasks
 
 
 if __name__ == "__main__":
@@ -109,7 +124,7 @@ if __name__ == "__main__":
     task_number = 10  # varied from 4 to 60, the task of one task set
     target_Uti = 0.5  # varied from 0.05 to 0.95
 
-    period_set = [10, 50, 100, 200, 500, 1000]
+    period_set = [10, 50, 100, 300, 600, 900]
     period = []
     for i in range(task_number):
         period.append(random.choice(period_set))
@@ -134,7 +149,10 @@ if __name__ == "__main__":
     # varied from 0.05 to 0.95
 
     task_set = task_definition(task_number, period, execution_time, criticality_factor, criticality_proportion)
-    table_print(task_set)
+    # print("-------------------- all tasks in the system--------------------")
+    # table_print(task_set)
+
+    # ------- LO_tasks and HI_tasks segmentation and Context aware task selection-----------#
 
     LO_tasks = []
     HI_tasks = []
@@ -144,15 +162,44 @@ if __name__ == "__main__":
         else:
             HI_tasks.append(i)
 
-    # table_print(LO_tasks)
-    # table_print(HI_tasks)
+    # ---  Context aware task selection ----#
+    LO_tasks = tasks_sort(LO_tasks, 0)
+    LO_tasks[0].criticality = "HI"
+    LO_tasks[0].context_aware = 1
+    LO_tasks[0].execution_time_HI = LO_tasks[0].execution_time_LO * criticality_factor
+    HI_tasks.append(LO_tasks[0])
+    LO_tasks.remove(LO_tasks[0])
 
-    # --- tasks dependency generator for LO_tasks--- #
+    HI_tasks = tasks_sort(HI_tasks, 0)
+    for i in range(len(HI_tasks)):
+        HI_tasks[i].app_id = 1680  # specific number
+        HI_tasks[i].task_id = i
 
-    LO_app1 = sample(LO_tasks, math.ceil(len(LO_tasks)/2))
-    for i in LO_app1:
-        LO_tasks.remove(i)
-    LO_app2 = LO_tasks
-    table_print(LO_app1)
-    table_print(LO_app2)
+    print("-------------------- HI tasks in the system--------------------")
+    table_print(HI_tasks)
+
+    # --- Tasks dependency generator for LO_tasks--- #
+
+    # step1: LO_application number 
+    LO_app_num = 2
+
+    # step2: tasks fragmentation
+    LO_apps = {}
+    tasks_perapp = math.ceil(len(LO_tasks) / LO_app_num)
+    for i in range(LO_app_num):
+        LO_apps[i] = sample(LO_tasks, tasks_perapp)
+        for j in LO_apps[i]:
+            LO_tasks.remove(j)
+            if len(LO_tasks) < tasks_perapp:
+                tasks_perapp = len(LO_tasks)
+    LO_apps_tasks = []
+    for i in range(len(LO_apps)):
+        LO_apps[i] = tasks_sort(LO_apps[i], i)
+        for j in LO_apps[i]:
+            LO_apps_tasks.append(j)
+    # you can check the tasks of LO_applications in different way
+    print("-------------------- LO tasks in the system--------------------")
+    table_print(LO_apps_tasks)
+
+    # step3:
 
